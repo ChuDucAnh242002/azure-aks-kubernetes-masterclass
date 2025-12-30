a409316410e3fd32b2e88365fce4a4f5b08f72b8
vboxuser <vboxuser@user.myguest.virtualbox.org>
2025-12-30 16:22:17 +0200

    Study DevOps and terraform

--- 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
+++ 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v2-terraform-azurerm-resource-group/main.tf
@@ -5,19 +5,20 @@ provider "azurerm" {
   #version = "=2.35.0"
   #version = "~>2.35.0"
   features {}
+  subscription_id = "fe496fba-d67f-421e-b553-8075cd0da353"
 }
 
  
 # Create a Azure Resource Group
 resource "azurerm_resource_group" "aks-rg2" {
   name     = "aks-rg2-tf"
-  location = "Central US"
+  location = "Sweden Central"
 
 # Add Tags
-#  tags = {
-#    "environment" = "k8sdev"
-#    "demotag"     = "refreshtest"
-#  }
+ tags = {
+   "environment" = "k8sdev"
+   "demotag"     = "refreshtest"
+ }
 
 }
 
